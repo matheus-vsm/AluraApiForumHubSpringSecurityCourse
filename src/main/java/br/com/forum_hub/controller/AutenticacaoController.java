158a7c4620efa5c6c69b12df9a855a3910d27119
matheus-vsm <scompinha@hotmail.com>
2026-05-21 22:57:47 -0300

    aula 2.4

--- src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
+++ src/main/java/br/com/forum_hub/controller/AutenticacaoController.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,8 @@ public class AutenticacaoController {
         this.authenticationManager = authenticationManager;
     }
 
-    public ResponseEntity<Authentication> efetuarLogin(@Valid DadosLogin dados) {
+    @PostMapping
+    public ResponseEntity<Authentication> efetuarLogin(@Valid @RequestBody DadosLogin dados) {
         var autenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
         var authentication = authenticationManager.authenticate(autenticationToken); // Executa o processo de autenticação usando o token (email e senha), retornando um objeto Authentication com os dados do usuário autenticado
 
