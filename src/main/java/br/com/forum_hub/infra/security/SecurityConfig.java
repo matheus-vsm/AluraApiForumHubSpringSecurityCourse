9823e93901929fa6602e8b0c548a7307dffa189e
matheus-vsm <scompinha@hotmail.com>
2026-05-21 22:02:11 -0300

    aula 2.3

--- src/main/java/br/com/forum_hub/infra/security/SecurityConfig.java
+++ src/main/java/br/com/forum_hub/infra/security/SecurityConfig.java
@@ -2,13 +2,25 @@ package br.com.forum_hub.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
+        return http
+                .sessionManagement(sm -> // Configura o gerenciamento de sessão da aplicação
+                        sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Define que a aplicação não criará nem usará sessões (stateless), comum em APIs REST que usam token (ex: JWT)
+                .csrf(csrf -> csrf.disable()) // Desativa a proteção CSRF (Cross-Site Request Forgery). Geralmente desabilitado em APIs stateless que não usam cookies para autenticação
+                .build(); // Constrói e retorna a cadeia de filtros de segurança configurada
+    }
 
     @Bean
     public PasswordEncoder encriptador() {
