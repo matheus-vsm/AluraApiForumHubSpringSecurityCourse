df8bf4e6717d6ed23bbd31f02c4b9e42995e851c
matheus-vsm <scompinha@hotmail.com>
2026-06-01 23:23:23 -0300

    aula 5.3

--- src/main/java/br/com/forum_hub/domain/perfil/Perfil.java
+++ src/main/java/br/com/forum_hub/domain/perfil/Perfil.java
@@ -1,14 +1,19 @@
 package br.com.forum_hub.domain.perfil;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "perfis")
-public class Perfil {
+public class Perfil implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private PerfilNome nome;
 
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + nome;
+    }
 }
